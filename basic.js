dd821377e1744282943ac7ffaf83e3197e2eab3d
siri <shirishaduggirala24@gmail.com>
2022-11-17 19:23:51 +0530

    edited by js file

--- basic.js
+++ basic.js
@@ -1,8 +1,10 @@
 function basic(){
-    let some1 = 10;
-    let some2 = 20;
+    let some1 = 100;
+    let some2 = 200;
     let some = some1 + some2;
+    let somex = some1 * some2;
   console.log(some);
+  console.log(somex);
 alert(some);
 }
 basic()
